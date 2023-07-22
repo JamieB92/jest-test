2c129aec724563c97f290ab489488a02da19203d
Jamie <j.bonner6992@gmail.com>
2023-07-22 15:18:53 +0100

    add new tests and add function to test dom

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -4,14 +4,21 @@
 
 const buttonClick = require("../button");
 
-beforeAll(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+// Use this as the boiler plate for every time your testing a functions and the dom as will check the whole page and run the tests you have built
+beforeEach(() => {
+    let fs = require("fs") 
+    let fileContents = fs.readFileSync("index.html", "utf-8") // Remeber to change to the page you wish to test
+    document.open();
+    document.write(fileContents)
+    document.close()
 });
 
 describe("DOM tests", () => {
     test("Expects content to change", () => {
         buttonClick();
-        expect(document.getElementById("par")
-            .innerHTML).toEqual("You Clicked");
+        expect(document.getElementById("par").innerHTML).toEqual("You Clicked"); // tests the button functioning
     });
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1) //finds the h1 and confirms theres only one 
+    })
 });
